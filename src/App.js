eec661d79ba01cf78d4ff12c2e48baf7252b8aa1
unknown <rmtestard@gmail.com>
2019-11-05 20:23:28 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useForm } from './useForm';
+
+// Only called on the first load
+function expensiveInitialState() {
+  return 10;
+}
 
 function App() {
+  const [count, setCount] = useState(() => expensiveInitialState());
+  const [{ num1, num2 }, setNum] = useState({ num1: 20, num2: 30 });
+
+  const [values, handleChange] = useForm({ email: '', password: '' });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{count}</div>
+
+      <button onClick={() => setCount(currentCount => currentCount + 1)}>
+        +
+      </button>
+      <div>{num1}</div>
+      <div>{num2}</div>
+      <button
+        onClick={() =>
+          setNum(currentState => ({
+            ...currentState,
+            num1: currentState.num1 + 1
+          }))
+        }>
+        +
+      </button>
+      <input name="email" value={values.email} onChange={handleChange} />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
     </div>
   );
 }
